c04865ac55865ddb7b7fe32054d9d32c0b12f9ce
iloveyellowcakes <daniellerukia@hotmail.com>
2022-05-30 08:01:02 -0300

    implementando forca

--- forca.py
+++ forca.py
@@ -4,20 +4,40 @@ def jogar():
     print("Bem vindo ao jogo da Forca")
     print(separador)
 
-    palavra_secreta = "banana"
+    palavra_secreta = "alface".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
     enforcou = False
-    nao_enforcou = False
+    acertou = False
+    erros = 0
 
-    while not enforcou and not nao_enforcou:
+    print(letras_acertadas)
+    while not enforcou and not acertou:
         chute = input("Qual a letra? ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        index = 0
-        for letra in palavra_secreta:
-            if chute.lower() == letra:
-                print(f"Encontrei a letra {chute} em posição {index}")
-            index = index + 1
+        if chute in palavra_secreta:
+            index = 0
+            for letra in palavra_secreta:
+                if chute == letra:
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+
+        exibe_resultado(acertou, enforcou)
+
     print("Fim do jogo")
+
+
+def exibe_resultado(acertou, enforcou):
+    if acertou:
+        return print("Parabéns você acertou a palavra")
+    elif enforcou:
+        return print("Você se tornou uma lastima da sociedade.")
 
 
 if __name__ == "__main__":
